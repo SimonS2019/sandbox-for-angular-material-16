2b891a9e1dfe60a59d74aefe2847b0fdbf2b0b58
SimonS2019 <shentiexiong@gmail.com>
2025-01-21 12:29:27 +1300

    add buttons module with routing and component implementation

--- sandbox-for-angular-material-16/src/app/app-routing.module.ts
+++ sandbox-for-angular-material-16/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'buttons', loadChildren: () => import('./buttons/buttons.module').then(m => m.ButtonsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
