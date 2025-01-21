e95936e0e960b42bca4dcebfeb62cc46bd3556d6
SimonS2019 <shentiexiong@gmail.com>
2025-01-21 14:01:02 +1300

    add cards module with routing and component implementation

--- sandbox-for-angular-material-16/src/app/app-routing.module.ts
+++ sandbox-for-angular-material-16/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'buttons', loadChildren: () => import('./buttons/buttons.module').then(m => m.ButtonsModule) }];
+const routes: Routes = [
+  {
+    path: 'buttons',
+    loadChildren: () =>
+      import('./buttons/buttons.module').then((m) => m.ButtonsModule),
+  },
+  {
+    path: 'cards',
+    loadChildren: () =>
+      import('./cards/cards.module').then((m) => m.CardsModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
