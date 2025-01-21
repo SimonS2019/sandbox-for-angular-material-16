f3c7ab35e49237ed92b9dbbc4852aa0d2fa135c2
SimonS2019 <shentiexiong@gmail.com>
2025-01-21 14:19:26 +1300

    add buttons component with various button styles and layout

--- sandbox-for-angular-material-16/src/app/buttons/buttons.module.ts
+++ sandbox-for-angular-material-16/src/app/buttons/buttons.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ButtonsRoutingModule } from './buttons-routing.module';
 import { ButtonsComponent } from './buttons.component';
-
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-  declarations: [
-    ButtonsComponent
-  ],
+  declarations: [ButtonsComponent],
   imports: [
     CommonModule,
-    ButtonsRoutingModule
-  ]
+    ButtonsRoutingModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+  ],
 })
-export class ButtonsModule { }
+export class ButtonsModule {}
