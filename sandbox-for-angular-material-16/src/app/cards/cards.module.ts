b7fdb620867e6a7ece310f182dc4af14bf6b1134
SimonS2019 <shentiexiong@gmail.com>
2025-01-21 14:17:59 +1300

    implement card component with layout, styles, and actions

--- sandbox-for-angular-material-16/src/app/cards/cards.module.ts
+++ sandbox-for-angular-material-16/src/app/cards/cards.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { CardsRoutingModule } from './cards-routing.module';
 import { CardsComponent } from './cards.component';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    CardsComponent
-  ],
-  imports: [
-    CommonModule,
-    CardsRoutingModule
-  ]
+  declarations: [CardsComponent],
+  imports: [CommonModule, CardsRoutingModule, MatCardModule, MatButtonModule],
 })
-export class CardsModule { }
+export class CardsModule {}
